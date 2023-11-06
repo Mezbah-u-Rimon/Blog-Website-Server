0b69c232d26bf09233b8552ab6b22d0663968ec7
Muzbah Uddin <rimonmr444@gmail.com>
2023-11-06 10:55:21 +0600

    added recent post and deploy vercel

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-//Blogs
-//53AXJQudWom68L35
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgd8wc9.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,12 +27,32 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const allBlogsCollection = await client.db('blogsDB').collection('allBlogs')
+
+
+        // recent blogs collection
+        app.get('/allBlogs', async (req, res) => {
+            const result = await allBlogsCollection.find().toArray();
+
+            const data = result.sort((a, b) => {
+                const dateA = new Date(`${a.date} ${a.time}`);
+                const dateB = new Date(`${b.date} ${b.time}`);
+                return dateB - dateA;
+            });
+
+            // console.log(data);
+            res.send(data);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
